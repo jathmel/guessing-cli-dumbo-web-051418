af73d8ac280eed6a358abbd9bbe1499e1caf5a5f
Jathmel Gordon <jathmel@gmail.com>
2018-05-12 15:55:45 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -4,10 +4,10 @@ def run_guessing_game
   actual_num = 1 + rand(6)
   puts 'Guess a number between 1 and 6.'
   guess = gets.chomp
-  case guess 
-    when 'exit'
-      puts 'Goodbye!'
-    else
+  # case guess 
+  #   when 'exit'
+  #     puts 'Goodbye!'
+  #   else
       if guess.to_i == actual_num
         puts 'You guessed the correct number!'
       elsif guess.to_i != actual_num
@@ -17,6 +17,6 @@ def run_guessing_game
       else
         puts 'Goodbye'
       end
-  end
+  # end
   
 end
